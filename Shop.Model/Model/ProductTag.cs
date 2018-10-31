e91d5495271fcb4935cd6346804a460a29af501f
huydangtruong <huydangtruong99190@gmail.com>
2018-10-31 22:51:22 +0700

    add domain entities

---
+++ Shop.Model/Model/ProductTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Model
+{
+    [Table("ProductTags")]
+    public class ProductTag
+    {
+        [Key]
+        public int ProductID { set; get; }
+        [Key]
+        public int TagID { set; get; }
+
+        [ForeignKey("ProductID")]
+        public Product Product { set; get; }
+        [ForeignKey("TagID")]
+        public Tag Tag { set; get; }
+    }
+}
---
+++ Shop.Model/Model/Slide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Model
+{
+    [Table("Slides")]
+    public class Slide
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Image { set; get; }
+        [Required]
+        public string Url { set; get; }
+        public int? OrderDisplay { set; get; }
+        [Required]
+        public bool Status { set; get; }
+    }
+}
